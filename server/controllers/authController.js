86c1072528df076c922a73c2a228bdfc194a1ed4
Ye Yint Aung <yeyintaung@agbcommunication>
2026-06-02 23:37:29 +0630

    [MODIFY] cors in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,8 +28,8 @@ const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+      secure: true,
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000,
     })
 
